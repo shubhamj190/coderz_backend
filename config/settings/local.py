8ff9f25d5ea0adb8df2077102a668743e9035620
shubham jadhav <shubhamjadhav190@gmail.com>
2025-03-18 21:33:56 +0530

    Update allowed hosts and CSRF trusted origins to include new domains

--- config/settings/local.py
+++ config/settings/local.py
@@ -46,7 +46,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "shubhamjadhav190@gmail.com"
 EMAIL_HOST_PASSWORD = "odei empj myvi wfko"
 
-ALLOWED_HOSTS=['viable-reasonably-goldfish.ngrok-free.app', 'localhost']
-CSRF_TRUSTED_ORIGINS=['https://viable-reasonably-goldfish.ngrok-free.app','http://localhost:3000']
+ALLOWED_HOSTS=['viable-reasonably-goldfish.ngrok-free.app', 'localhost','coding1.questplus.in','coding2.questplus.in']
+CSRF_TRUSTED_ORIGINS=['https://viable-reasonably-goldfish.ngrok-free.app','http://localhost:3000','http://coding1.questplus.in','http://coding2.questplus.in']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
