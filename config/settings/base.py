c823eb4dcf42c8e93e9388a52efda96c9de314ea
shubham jadhav <shubhamjadhav190@gmail.com>
2025-03-27 23:07:37 +0530

    Update CORS_ALLOWED_ORIGINS to allow all origins for broader access.

--- config/settings/base.py
+++ config/settings/base.py
@@ -164,8 +164,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://yourfrontend.com", 
-]
+CORS_ALLOWED_ORIGINS = ['*']
